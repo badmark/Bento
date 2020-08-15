dd8e489b032aa6f3a7ab653974c18c7ff66f94db
Mark Buckman <badmark@gmail.com>
2020-08-15 17:51:43 -0400

    Update time.js

--- js/time.js
+++ js/time.js
@@ -20,10 +20,17 @@ function displayClock() {
     var dd = d.getDate();
     var min = (mins = ('0' + d.getMinutes()).slice(-2));
     var hh = d.getHours();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
 
-    document.getElementById('hour').innerText = hh;
+    document.getElementById('hour').innerText = hours;
     document.getElementById('separator').innerHTML = ' : ';
-    document.getElementById('minutes').innerText = min;
+    document.getElementById('minutes').innerText = minutes + ' ' + ampm;
 
     document.getElementById('month').innerText = mm;
     document.getElementById('day').innerText = dd;
